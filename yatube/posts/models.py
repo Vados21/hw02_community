0d64f4da78e11a96107d5af5a39411a9f3d14e55
Vados21 <vadim.shpilevskiy@yandex.ru>
2021-10-08 14:21:41 +0300

    project_after_review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -10,7 +10,7 @@ class Group(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return str(self.title)
+        return self.title
 
 
 class Post(models.Model):
@@ -23,6 +23,9 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group, blank=True, null=True,
-        on_delete=models.CASCADE,
-        related_name='group'
+        on_delete=models.SET_NULL,
+        related_name='posts'
     )
+
+    class Meta:
+        ordering = ['-pub_date']
